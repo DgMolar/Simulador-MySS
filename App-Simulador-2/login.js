7b126b33ffd789a35b7c9421d2caa2c6b4b1272d
diego <diego@DGMOLAR-ACER>
2023-11-12 22:28:42 -0600

    si

--- App-Simulador-2/login.js
+++ App-Simulador-2/login.js
@@ -1,8 +1,4 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-
-const dbPath = path.join(__dirname, 'MyDatabase.db');
-const db = new sqlite3.Database(dbPath);
+const { ipcRenderer } = require('electron');
 
 const form = document.getElementById('login-form');
 
@@ -13,13 +9,13 @@ form.addEventListener('submit', async (e) => {
 
   try {
     const isAuthenticated = await authUser(usernameInput.value, passwordInput.value);
-    if(isAuthenticated){
+    if (isAuthenticated) {
       alert("Usuario autenticado. Acceso permitido.");
-      location.reload();
+      // transferir a la pagina principal.
+      location.href = 'index.html';
     } else {
       alert("Usuario o contraseña incorrectos");
     }
-    //console.log(isAuthenticated ? 'Usuario autenticado. Acceso permitido.' : 'Usuario no autenticado. Acceso denegado.');
   } catch (error) {
     console.error('Error durante la autenticación:', error);
   }
@@ -27,12 +23,13 @@ form.addEventListener('submit', async (e) => {
 
 function authUser(username, password) {
   return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM usuarios WHERE username = ? AND password = ?', [username, password], (err, row) => {
-      if (err) {
-        console.error('Error al autenticar usuario:', err);
-        reject(err);
+    ipcRenderer.send('auth-request', { username, password });
+
+    ipcRenderer.once('auth-response', (event, data) => {
+      if (data.error) {
+        reject(new Error(data.error));
       } else {
-        resolve(!!row); // Simplifica la lógica para devolver true o false directamente
+        resolve(data.isAuthenticated);
       }
     });
   });
